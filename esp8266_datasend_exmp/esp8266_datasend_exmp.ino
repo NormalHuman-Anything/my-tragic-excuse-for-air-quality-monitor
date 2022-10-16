1424bb696d6e708b61b701f178d577794ed8e1d1
NormalHuman <72422431+NormalHuman-Anything@users.noreply.github.com>
2022-10-17 00:59:28 +0900

    it works!

--- esp8266_datasend_exmp/esp8266_datasend_exmp.ino
+++ esp8266_datasend_exmp/esp8266_datasend_exmp.ino
@@ -1,40 +1,95 @@
+/**
+   BasicHTTPClient.ino
+
+    Created on: 24.05.2015
+
+*/
+
+#include <Arduino.h>
+
 #include <ESP8266WiFi.h>
+#include <ESP8266WiFiMulti.h>
+
 #include <ESP8266HTTPClient.h>
 
-char* wifi_ssid = "UCA";
-char* password = "0527080001"
+#include <WiFiClient.h>
+
+ESP8266WiFiMulti WiFiMulti;
+String serverName = "http://192.168.0.6:8000/name/";
+String AGE = "/age/";
+int age=0;
+int namexmp=0;
+char link;
+
 
 void setup() {
-  // put your setup code here, to run once:
+
   Serial.begin(115200);
-  WiFi.begin(wifi_name, password);
-  while (WiFi.status() != WL_CONNECTED) {
+  // Serial.setDebugOutput(true);
+
+  Serial.println();
+  Serial.println();
+  Serial.println();
+
+  for (uint8_t t = 4; t > 0; t--) {
+    Serial.printf("[SETUP] WAIT %d...\n", t);
+    Serial.flush();
     delay(1000);
-    Serial.print("Connecting...");
   }
+
+  WiFi.mode(WIFI_STA);
+  WiFiMulti.addAP("123456", "ppap1542xd");
+
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-  if (WiFi.status() == WL_CONNECTED) {
-    HTTPClient http = "http://127.0.0.1:8000";
+  // wait for WiFi connection
+  if ((WiFiMulti.run() == WL_CONNECTED)) {
 
-    String name = "wonjun";
-    String age = "50";
+    WiFiClient client;
 
-    getRequestURL += "/name/"; //http://127.0.0.1:8000/name/
-    getRequestURL += name; //http://127.0.0.1:8000/name/wonjun
-    getRequestURL += "/age/"; //http://127.0.0.1:8000/name/wonjun/age/
-    getRequestURL += age; //http://127.0.0.1:8000/name/wonjun/age/50
+   
+   
+      HTTPClient http;
 
-    http.begin(getRequestURL);
+      Serial.print("[HTTP] begin...\n");
+      age=random(1, 100);
+      namexmp=random(1, 100);
+      delay(100);
+      String serverPath = serverName + namexmp + AGE + age;
+      if (http.begin(client, serverPath.c_str())) {  // HTTP
 
-    int httpCode = http.GET();
-    if (httpCode > 0) {
-      String payload = http.getString();
-      Serial.println(payload);
+
+        Serial.print("[HTTP] GET...\n");
+      // start connection and send HTTP header
+        int httpCode = http.GET();
+
+      // httpCode will be negative on error
+        if (httpCode > 0) {
+        // HTTP header has been send and Server response header has been handled
+          Serial.printf("[HTTP] GET... code: %d\n", httpCode);
+
+        // file found at server
+        if (httpCode == HTTP_CODE_OK || httpCode == HTTP_CODE_MOVED_PERMANENTLY) {
+          String payload = http.getString();
+          Serial.println(payload);
+        }
+      } else {
+        Serial.println(serverPath.c_str());
+        Serial.printf("[HTTP] GET... failed, error: %s\n", http.errorToString(httpCode).c_str());
+      }
+
+      http.end();
+    } else {
+      Serial.println(serverPath.c_str());
+      Serial.printf("[HTTP} Unable to connect\n");
     }
-    http.end();
+    
+    
+
+    
+    
   }
-  delay(3000);
+
+  delay(100);
 }
